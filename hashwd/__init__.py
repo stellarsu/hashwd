2fda719047dc1cc5554d890de95a839d255416c3
Jordan Langland <97185219+stellarsu@users.noreply.github.com>
2023-01-27 13:07:29 -0500

    updated circular import for parameters

--- hashwd/__init__.py
+++ hashwd/__init__.py
@@ -2,7 +2,7 @@
 
 
 from pyperclip import copy
-from hashwd import paramaters
-from hashwd.parameters import MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MAX_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, SYMBOLS, DICTIONARY_FILE, WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT
 from hashwd import generate
 from hashwd import clear
+from hashwd import paramaters
+from parameters import MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MAX_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, SYMBOLS, DICTIONARY_FILE, WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT
