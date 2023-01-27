b324345e183cfbc4166a131966f873d8763b855c
Jordan Langland <97185219+stellarsu@users.noreply.github.com>
2023-01-27 12:38:06 -0500

    updated file imports

--- hashwd/__init__.py
+++ hashwd/__init__.py
@@ -2,7 +2,7 @@
 
 
 from pyperclip import copy
-from hashwd import pass_defaults
-from hashwd.pass_defaults import MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MAX_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, SYMBOLS, DICTIONARY_FILE, WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT
-from hashwd import hashwd
-from hashwd.hashwd import generate_password, clear_clipboard
+from hashwd import paramaters
+from hashwd.parameters import MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MAX_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, SYMBOLS, DICTIONARY_FILE, WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT
+from hashwd import generate
+from hashwd import clear
