3d82e0511e399cc659f03b36f4326a9f5857bc68
stellarsu <root@gnom.me>
2023-02-09 01:24:46 -0500

    minor update

--- hashwd/__main__.py
+++ hashwd/__main__.py
@@ -9,7 +9,7 @@ import parameters as parameters
 
 
 # Define functions to handle the commands
-def set_parameters():
+def main():
     # Get the directory of your script
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -210,4 +210,4 @@ def set_parameters():
 
 # Run the password_parameters function
 if __name__ == "__main__":
-    set_parameters()
+    main()
