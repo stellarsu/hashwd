d7d1c18c9c1711222f2a5047b0c162b0481b9bc2
stellarsu <root@gnom.me>
2023-02-09 01:23:45 -0500

    resyncing

--- hashwd/__init__.py
+++ hashwd/__init__.py
@@ -5,14 +5,8 @@ from generate import generate_password
 from clear import clear_clipboard
 from parameters import MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MAX_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, SYMBOLS, DICTIONARY_FILE, WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT
 from pyperclip import copy
-<<<<<<< Updated upstream
-from generate import generate_password
-from clear import clear_clipboard
-from parameters import MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, MAX_SYMBOLS, DICTIONARY_FILE, WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT
-=======
 from random import randint, random, sample
 from argparse import ArgumentParser
 from sys import exit
 from os import path
 from re import match
->>>>>>> Stashed changes
